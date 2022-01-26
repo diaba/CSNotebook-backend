ae66b70722690ec36da093cf68630c9917adc19d
Michael Croghan <mcrog.7@gmail.com>
2022-01-26 18:48:45 -0500

    Initialized Spring Boot Project

---
+++ src/main/java/com/backend/csnotebook/CsNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.backend.csnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsNotebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsNotebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/csnotebook/CsNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.csnotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsNotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
