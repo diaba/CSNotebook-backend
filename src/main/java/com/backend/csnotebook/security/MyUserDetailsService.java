c30c2012a2619f94a8632de245136aaf39bc95d4
Michael Croghan <mcrog.7@gmail.com>
2022-01-27 14:54:43 -0500

    Create MyUserDetailsService Clas in Security Package

---
+++ src/main/java/com/backend/csnotebook/security/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.backend.csnotebook.security;
+
+import com.backend.csnotebook.model.User;
+import com.backend.csnotebook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/** The MyUserDetailsService class helps to load user-specific data. */
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
+}
