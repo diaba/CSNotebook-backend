0da32d1fb5c533c750bbc25bc452faaa64edb5e9
Michael Croghan <mcrog.7@gmail.com>
2022-01-27 15:05:47 -0500

    Complete LoginResponse Class in Model/Auth/Response Package

--- src/main/java/com/backend/csnotebook/model/auth/response/LoginResponse.java
+++ src/main/java/com/backend/csnotebook/model/auth/response/LoginResponse.java
@@ -1,4 +1,30 @@
 package com.backend.csnotebook.model.auth.response;
 
+/** The LoginResponse class maintains data and methods related to Log-in Response objects.
+ */
 public class LoginResponse {
+    /** JSON Web Token Property */
+    private String jwtToken;
+
+    /** LoginResponse Constructor.
+     * @param jwtToken the JSON web token.
+     */
+    public LoginResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    /** Returns the JWT.
+     * @return JSON Web Token.
+     */
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+
+    /** Sets the JSON Web Token in the LoginResponse.
+     * @param jwtToken the JWT to set in the response.
+     */
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
 }
