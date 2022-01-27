e27b7277c69ea2127ba5a9a5ec4491f2dc419fb8
Michael Croghan <mcrog.7@gmail.com>
2022-01-27 15:47:15 -0500

    Create TopicService Class in the Service Package

--- src/main/java/com/backend/csnotebook/controller/TopicController.java
+++ src/main/java/com/backend/csnotebook/controller/TopicController.java
@@ -1,9 +1,13 @@
 package com.backend.csnotebook.controller;
 
 import com.backend.csnotebook.model.Topic;
+import com.backend.csnotebook.service.TopicService;
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 /** The TopicController class serves as the controller for managing the flow of data concerning Topics. */
@@ -13,5 +17,16 @@ public class TopicController {
 
     private TopicService topicService;
     private static final Logger LOGGER = Logger.getLogger(TopicController.class.getName());
+
+    @Autowired
+    public void setTopicService(TopicService topicService){
+        this.topicService = topicService;
+    }
+
+    // GET ALL TOPICS (localhost:9092/api/topics)
+    public List<Topic> getAllTopics(){
+        LOGGER.info("getAllTopics() Method called from TopicController!");
+        return topicService.getAllDays();
+    }
 
 }
---
+++ src/main/java/com/backend/csnotebook/service/TopicService.java
@@ -0,0 +1,15 @@
+package com.backend.csnotebook.service;
+
+import com.backend.csnotebook.model.Topic;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/** The TopicService class responsible for handling business logic related to Topics. */
+@Service
+public class TopicService {
+    
+
+    public List<Topic> getAllDays() {
+    }
+}
